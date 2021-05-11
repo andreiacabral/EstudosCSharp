bf3e4aebff0e4702f349cc69ea36014185b1b260
Andreia Cabral <andreiacabraldacosta@outlook.com>
2021-05-11 10:41:48 -0300

    Adicionado remoção de produtos

--- Projeto2Classes/Projeto2Classes/Program.cs
+++ Projeto2Classes/Projeto2Classes/Program.cs
@@ -18,9 +18,29 @@ namespace Projeto2Classes
             p.Preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
             Console.Write("Quantidade no estoque: ");
             p.Quantidade = int.Parse(Console.ReadLine());
-
+            Console.WriteLine();
             Console.WriteLine("Dados do produto: " + p);
 
+            Console.WriteLine();
+            Console.Write("Digite o numero de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.ReadLine();
+
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o numero de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.ReadLine();
+
+            Console.WriteLine("Dados atualizados: " + p);
+
+
         }
+
+
     }
 }
+
