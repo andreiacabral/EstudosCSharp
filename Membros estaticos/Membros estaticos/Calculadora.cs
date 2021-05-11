2b42a9ba7ad2ae9dfa77128c8d88c6815ae1f2b4
Andreia Cabral <andreiacabraldacosta@outlook.com>
2021-05-11 15:56:27 -0300

    Membros Estáticos-parte 2

---
+++ Membros estaticos/Membros estaticos/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Membros_estaticos
+{
+    class Calculadora
+    {
+        public static double Pi = 3.14;
+
+         public static double Circuferencia(double raio)
+        {
+            return 2.0 * Pi * raio;
+        }
+
+        public static double Volume(double raio)
+        {
+            return 4.0 / 3.0 * Pi * raio * raio * raio;
+        }
+
+    }
+}
--- Membros estaticos/Membros estaticos/Program.cs
+++ Membros estaticos/Membros estaticos/Program.cs
@@ -3,34 +3,20 @@ using System.Globalization;
 
 namespace Membros_estaticos
 {
-    class Program { 
-
-        static double Pi = 3.14;
-
-
-    
+    class Program
+    {
         static void Main(string[] args)
         {
             Console.WriteLine("Entre com valor de raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circ = Circuferencia(raio);
-            double volume = Volume(raio);
+            double circ = Calculadora. Circuferencia(raio);
+            double volume = Calculadora.Volume(raio);
 
             Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Volume: " + volume.ToString("F2",CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de Pi: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
 
-        }
-
-        static double Circuferencia(double raio)
-        {
-            return 2.0 * Pi * raio;
-        }
-
-        static double Volume(double raio)
-        {
-            return 4.0 / 3.0 * Pi * raio * raio * raio;
         }
     }
 }
