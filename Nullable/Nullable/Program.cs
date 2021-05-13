0e8823fc8943c668a253f4db588ba3ad988c41dc
Andreia Cabral <andreiacabraldacosta@outlook.com>
2021-05-13 11:24:10 -0300

    Nullable

---
+++ Nullable/Nullable/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = null;
+            double? y = 10.00;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            //Console.WriteLine(x.Value);
+            Console.WriteLine(y.Value);
+
+            if (x.HasValue)
+                Console.WriteLine(x.Value);
+
+            else
+                Console.WriteLine(x is null);
+            Console.WriteLine(y is null);
+
+            if (y.HasValue)
+                Console.WriteLine(y.Value);
+
+            else
+                Console.WriteLine(y.Value);
+
+        }
+    }
+}
