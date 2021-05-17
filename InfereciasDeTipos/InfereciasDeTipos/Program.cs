b4d8b5871740a5bc4ee2d9731e36b5947a236855
Andreia Cabral <andreiacabraldacosta@outlook.com>
2021-05-17 13:49:17 -0300

    Inferências do Tipo da palavra Var

---
+++ InfereciasDeTipos/InfereciasDeTipos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InfereciasDeTipos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
