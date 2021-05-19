9305fbc7b5bcae7fd9fa5b06ea9d08d82264f9ef
Andreia Cabral <andreiacabraldacosta@outlook.com>
2021-05-19 18:54:47 -0300

    Funções Interessantes para String

---
+++ expressãoTernaria/expressãoTernaria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace expressãoTernaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
