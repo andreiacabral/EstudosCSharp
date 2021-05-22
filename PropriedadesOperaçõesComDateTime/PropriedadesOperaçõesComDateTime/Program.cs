9ec02f07ec99b1f65aae309466ae5844f1e8ea7d
Andreia Cabral <andreiacabraldacosta@outlook.com>
2021-05-22 11:09:00 -0300

    Propriedades e operações com DateTime 2

---
+++ PropriedadesOperaçõesComDateTime/PropriedadesOperaçõesComDateTime/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PropriedadesOperaçõesComDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d = new DateTime(2001, 8, 15, 13, 45, 58);
+
+            string s1 = d.ToLongTimeString();
+            string s2 = d.ToShortDateString();
+            string s3 = d.ToShortTimeString();
+            string s4 = d.ToString();
+            string s5 = d.ToString("yyyy-MM-dd-HH:mm:ss");
+            string s6 = d.ToString("yyyy-MM-dd-HH:mm:fff");
+
+
+            Console.WriteLine(d.ToLongDateString());
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+            Console.WriteLine(s5);
+            Console.WriteLine(s6);
+        }
+        
+    }
+}
