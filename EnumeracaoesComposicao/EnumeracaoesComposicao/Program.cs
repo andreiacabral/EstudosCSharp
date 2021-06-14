6640940d53ff1b368c05fc1c92a36d426c50d450
Andreia Cabral <andreiacabraldacosta@outlook.com>
2021-06-13 22:22:39 -0300

    Herança

---
+++ EnumeracaoesComposicao/EnumeracaoesComposicao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnumeracaoesComposicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
