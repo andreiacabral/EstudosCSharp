5d71188da704534605e57d7b798aa2aaf363b5b5
Andreia Cabral <andreiacabraldacosta@outlook.com>
2021-06-15 10:27:10 -0300

    Sobreposição, palavras virtual, override e base

--- Heranca/Heranca/Entities/Account.cs
+++ Heranca/Heranca/Entities/Account.cs
@@ -18,18 +18,14 @@
             Holder = holder;
             Balance = balance;
         }
-
-        public void Withdraw(double amount )
+        public virtual void Withdraw(double amount)
         {
-            Balance -= amount;
+            Balance -= amount + 5.0;
         }
 
         public void Deposit(double amount)
         {
-            Balance += amount;
-
+            Balance -= amount;
         }
-
-
     }
 }
--- Heranca/Heranca/Program.cs
+++ Heranca/Heranca/Program.cs
@@ -7,10 +7,14 @@ namespace Heranca
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+            Account acc1 = new Account(1001, "Alex", 500.0);
+            Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
 
+            acc1.Withdraw(10.0);
+            acc2.Withdraw(10.0);
 
-            Console.WriteLine(account.Balance);
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
         }
     }
 }
